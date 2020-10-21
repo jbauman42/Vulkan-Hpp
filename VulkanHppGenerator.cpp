6e4df8609d7e1d87f9bdb71857809b59cdbf2d4f
John Bauman <jbauman@google.com>
2020-10-21 21:28:18 +0000

    Add std::move to createResult
    Otherwise createGraphicsPipeline would fail.

--- VulkanHppGenerator.cpp
+++ VulkanHppGenerator.cpp
@@ -8735,7 +8735,7 @@ namespace std
       throwResultException( result, message );
     }
 #endif
-    return ResultValue<T>( result, data );
+    return ResultValue<T>( result, std::move( data ) );
   }
 
 #ifndef VULKAN_HPP_NO_SMART_HANDLE
